f63cb7335919a17fdfcf53f3c338935a8b8efc36
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2025-01-31 18:59:21 -0600

    Refactor EjecutarProgramaSO to use ProcessBuilder for launching Notepad

--- src/main/java/com/zutjmx/curso/java/comun/EjecutarProgramaSO.java
+++ src/main/java/com/zutjmx/curso/java/comun/EjecutarProgramaSO.java
@@ -1,30 +1,45 @@
 package com.zutjmx.curso.java.comun;
 
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+//import java.io.IOException;
 
 public class EjecutarProgramaSO {
     public void ejecutar() {
         System.out.println("EjecutarProgramaSO.ejecutar()");
-        Runtime runtime = Runtime.getRuntime();
-        Process process;
-        if (System.getProperty("os.name").startsWith("Windows")) {
-            try {
-                process = runtime.exec("notepad");
+        
+        // creating list of process
+        List<String> list = new ArrayList<String>();
+        list.add("notepad.exe");
+ 
+        // create the process
+        ProcessBuilder build = new ProcessBuilder(list);
+ 
+        // checking the command i list
+        System.out.println("command: " + build.command());
+
+        // Runtime runtime = Runtime.getRuntime();
+        // Process process;
+        // if (System.getProperty("os.name").startsWith("Windows")) {
+        //     try {
+        //         process = runtime.exec("notepad");
                 
-                try {
-                    process.waitFor();
-                } catch (InterruptedException e) {
-                    System.err.println("Error al tratar de ejecutar notepad: ".concat(e.getMessage()));;
-                    System.exit(1);
-                }
+        //         try {
+        //             process.waitFor();
+        //         } catch (InterruptedException e) {
+        //             System.err.println("Error al tratar de ejecutar notepad: ".concat(e.getMessage()));;
+        //             System.exit(1);
+        //         }
 
-                System.out.println("Se cerró el editor");
-                System.exit(0);
+        //         System.out.println("Se cerró el editor");
+        //         System.exit(0);
 
-            } catch (IOException e) {                
-                System.err.println("Error al tratar de ejecutar notepad: ".concat(e.getMessage()));;
-                System.exit(1);
-            }
-        }
+        //     } catch (IOException e) {                
+        //         System.err.println("Error al tratar de ejecutar notepad: ".concat(e.getMessage()));;
+        //         System.exit(1);
+        //     }
+        // }
+
     }
 }
